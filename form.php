946e9556a98a4d551f0a6c19d841fe5c3ef85377
Drew Richards <98420057+darichards10@users.noreply.github.com>
2023-05-01 17:26:39 -0400

    Entire project

---
+++ form.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="styles.css">
+
+        <title>
+             Weather App
+        </title>
+    </head>
+    <body>
+        <div class="form-container">
+            <H1>Weather App</H1>
+        <form action="index.php" method="post">
+            <p>Enter City: <input type="text" name="city"></p>
+            <button>Submit</button>
+        </form>
+        </div>
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" href="styles.css">
+        
+        <title>
+        Weather App 
+        </title>
+    </head>
+    <body>
+        <div class="form-container">
+           
+        <?php
+        $apiKey = "21f569c00535598be3ffa1d55383da35";
+
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $city = $_POST['city'];
+            $url = "https://api.openweathermap.org/data/2.5/weather?q=" . $city . "&units=imperial&appid=" . $apiKey;
+            $response = file_get_contents($url);
+
+            $data = json_decode($response, true);
+        }
+        $main = $data['main']; //main in json 
+        $weather = $data['weather']; //weather in json
+
+        $temp = $main['temp'];
+
+        $string = json_encode($data, JSON_PRETTY_PRINT); //shows all weather data
+
+        
+        date_default_timezone_set('America/New_York');
+        $cur_time = date('h:i A');
+        ?>
+        <h1><?php echo $city; ?> </h1>
+        <p>Feels like <?php echo $main['feels_like']; ?> *F</p>
+        <div class="cols">
+        <ul class="col">
+            <li> Temp: <?php echo $main['temp']; ?> *F</li>
+            <li> Max Temp: <?php echo $main['temp_max']; ?> *F</li>
+            <li> Min Temp: <?php echo $main['temp_min']; ?> *F</li>                                                                                                                                                                                                                                                                                                                                                       
+        </ul>
+        <ul class="col">
+            <li> Pressure: <?php echo $main['pressure']; ?></li>
+            <li> Humidity: <?php echo $main['humidity']; ?> %</li>
+            <li> Wind Spd: <?php echo $data['wind']['speed']; ?> MPH</li>                                                                                                                                                                                                                                                                                                                                                       
+        </ul >
+        </div>
+        <p>Lon, Lat: <?php echo $data['coord']['lon']?>, <?php echo $data['coord']['lat']?></p>
+        <p> Data as of <?php echo $cur_time; ?> </p>
+        </div>
+        <div class="form-container">
+            <form method="post" action="index.php">
+                <p>Enter City <input name="city" type="text"></p>
+                <button>Submit</button>
+            </form>
+        </div>
+    </body>
+</html>
